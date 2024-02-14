cc3bf400f8b80b4e38611b6a3d661dc3e5acea0c
6urk <82283209+6urk@users.noreply.github.com>
2024-02-13 21:14:09 -0600

    getting a 404 eror on /expenses

--- expensemanager/src/main/java/controller/ExpenseController.java
+++ expensemanager/src/main/java/controller/ExpenseController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ExoenseControler {
+public class ExpenseController {
+
     @GetMapping("/expenses")
-    public String showExpenses(){
+    public String showExpenseList() {
         return "expenses-list";
     }
 }
+// Path: expensemanager/src/main/resources/templates/expenses-list.html
